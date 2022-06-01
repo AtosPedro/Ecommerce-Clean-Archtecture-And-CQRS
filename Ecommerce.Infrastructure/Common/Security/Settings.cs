7c964a7aff429b3dda1b7175183eec34a615cd24
atos.pedro <atos.pedro@vegatech.com.br>
2022-06-01 16:18:20 -0300

    removendo diretivas using desnecessárias

--- Ecommerce.Infrastructure/Common/Security/Settings.cs
+++ Ecommerce.Infrastructure/Common/Security/Settings.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Ecommerce.Infrastructure.Common.Security
+﻿namespace Ecommerce.Infrastructure.Common.Security
 {
     public class Settings
     {
