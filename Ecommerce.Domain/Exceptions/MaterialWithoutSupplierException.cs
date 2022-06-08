facf1a84edb31c4df6fe2a58dd6558781b5226e2
atos.pedro <atos.pedro@vegatech.com.br>
2022-06-08 15:47:24 -0300

    Adicionando os projetos de WebUI, DesktopUI E Infrastructure.IntegrationTests

--- Ecommerce.Domain/Exceptions/MaterialWithoutSupplierException.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Ecommerce.Domain.Exceptions
-{
-    public class MaterialWithoutSupplierException : Exception
-    {
-        private const string _message = "O produto não possui um fornecedor";
-        public MaterialWithoutSupplierException(string message = _message) : base (message){}
-    }
-}
