c7e3ab9ebf9dcbd26e01c339dffb5913cdf92dbd
atos.pedro <atos.pedro@vegatech.com.br>
2022-06-09 13:26:06 -0300

    removendo as referências do fluent validation no projeto da api

--- Ecommerce.Api/Program.cs
+++ Ecommerce.Api/Program.cs
@@ -1,15 +1,10 @@
 using Ecommerce.Application.Common.Extensions;
 using Ecommerce.Infrastructure.Common.Extensions;
-using FluentValidation.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers().AddFluentValidation(
-    config =>
-    {        
-        config.RegisterValidatorsFromAssemblyContaining<Program>();
-    });
+builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
