8a6f40d4e1b4360e11949f62fbbcfa5e1cc18b4d
atos.pedro <atos.pedro@vegatech.com.br>
2022-06-14 16:08:56 -0300

    Adding the dependencies in the CreateOperationCommand.cs

--- Ecommerce.Application/Operations/Commands/CreateOperation/CreateOperationCommand.cs
+++ Ecommerce.Application/Operations/Commands/CreateOperation/CreateOperationCommand.cs
@@ -1,5 +1,7 @@
-﻿using Ecommerce.Application.Common.Communication;
+﻿using AutoMapper;
+using Ecommerce.Application.Common.Communication;
 using Ecommerce.Application.Common.DTOs.Operations;
+using Ecommerce.Application.Common.Interfaces;
 
 namespace Ecommerce.Application.Operations.Commands.CreateOperation
 {
@@ -10,6 +12,17 @@ namespace Ecommerce.Application.Operations.Commands.CreateOperation
 
     public class CreateOperationCommandHandler : IHandlerWrapper<CreateOperationCommand, ReadOperationDto>
     {
+        private readonly IOperationRepository _operationRepository;
+        private readonly IMapper _mapper;
+
+        public CreateOperationCommandHandler(
+            IOperationRepository operationRepository, 
+            IMapper mapper)
+        {
+            _operationRepository = operationRepository;
+            _mapper = mapper;
+        }
+
         public Task<Response<ReadOperationDto>> Handle(CreateOperationCommand request, CancellationToken cancellationToken)
         {
             throw new NotImplementedException();
